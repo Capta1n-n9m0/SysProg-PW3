cfd5c020e9169ff91d7a74f5b915facbe1f1605b
Capta1n-n9m0 <alievabbas1@gmail.com>
2021-12-01 21:28:05 +0400

    ex2 is done

--- ex2.c
+++ ex2.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
+int my_getchar();
+
 int main(){
     puts("Ex2");
+    int c;
+    while((c = my_getchar()) != EOF){
+        printf("%c", c);
+    }
     return 0;
+}
+
+int my_getchar(){
+    // lol, i can use getch here, can't i? :D
+    char buf;
+    fread(&buf, sizeof(char), 1, stdin);
+    if(buf == 0) return EOF;
+    return (int)buf;
 }
